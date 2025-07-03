e1994ee7eedc028cb580e998ffc43e6a18b22570
Pratham Chauhan <136956315+chauhan-pratham@users.noreply.github.com>
2025-07-03 14:10:14 +0530

    Delete audit-root directory

--- audit-root/scripts/deploy.js
+++
@@ -1,7 +0,0 @@
-async function main() {
-    const AuditRootStorage = await ethers.getContractFactory("AuditRootStorage");
-    const contract = await AuditRootStorage.deploy();
-    await contract.deployed();
-    console.log("Contract deployed at:", contract.address);
-}
-main();
